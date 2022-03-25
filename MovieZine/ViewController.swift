6ecc6f24fb4108a9a93f2cc36f3f11a22ffd9bf4
adityagi02 <adityatyagi20bcs126@iiitkottayam.ac.in>
2022-03-25 16:56:52 +0530

    Initial Commit

---
+++ MovieZine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieZine
+//
+//  Created by Aditya Tyagi  on 25/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
